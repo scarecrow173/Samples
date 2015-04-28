6c3a66c26f26fd4a7070298256efd6e2ef308274
scarecrow173 <akiyoshi.koyama@gmail.com>
2015-04-28 19:20:59 +0900

    [Add]TheShowshankRedmption

---
+++ TheShawshankRedemption/TheShawshankRedemption/Classes/History.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TheShawshankRedemption.Classes
+{
+    [XmlRoot("History")]
+    public class History
+    {
+        public static History GetInstance { get { Instance = Instance != null ? Instance : new History(); return Instance; } }
+        private static History Instance = null;
+
+        private static string FileExtention { get { return "xml"; } }
+        private static string FileName { get { return "History" + "." + FileExtention; } }
+
+        [XmlElement("HistoryList")]
+        private List<string> HistoryList { get; set; }
+
+        private Stack<string> HistoryStack = new Stack<string>();
+
+        public bool IsLoaded = false;
+
+        private History()
+        {
+            HistoryList = new List<string>();
+            LoadHistory();
+        }
+
+        public void SaveHistory()
+        {
+            HistoryList = HistoryStack.ToList();
+            //TODO:ここにSave処理
+
+        }
+        
+        public void LoadHistory()
+        {
+            //TODO:ここにLoad処理
+
+
+            foreach(string iHistoryStr in HistoryList)
+            {
+                HistoryStack.Push(iHistoryStr);
+            }
+        }
+    }
+}
---
+++ TheShawshankRedemption/TheShawshankRedemption/Pages/BrowserPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Xml.Serialization;
+namespace TheShawshankRedemption.Pages
+{
+    /// <summary>
+    /// Interaction logic for BrowserPage.xaml
+    /// </summary>
+    public partial class BrowserPage : UserControl
+    {
+
+        public BrowserPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+        }
+        private void Front_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StackPanel_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Browser.Height = e.NewSize.Height - MenuPanel.RenderSize.Height;
+        }
+
+        private void URL_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Focusされていなければ無視
+            if (!URLBox.IsFocused)
+                return;
+            // EnterKeyでなければ無視
+            if (e.Key != Key.Return)
+                return;
+            // 現在と同じページであれば無視
+            if (Browser.Source.ToString() == URLBox.Text)
+                return;
+
+            Browser.Source = new Uri(URLBox.Text);
+            UpdateHistory(URLBox.Text);
+        }
+        private void UpdateHistory(string URL)
+        {
+
+        }
+
+    }
+}
